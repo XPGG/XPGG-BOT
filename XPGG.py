ed531d73ba49ccfcf689b4b0a9da16b7118b9185
jukaiof <reaju.sh1.ne.jp@gmail.com>
2018-02-14 06:19:03 +0900

    リストの場所変えたよ

--- XPGG.py
+++ XPGG.py
@@ -14,12 +14,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+#test0
     if client.user != message.author:  #他BOT回避
+        channels = ["398447971018211329", "394434155443650560", "398446399005655051"]
+                Xchannels = [
+                    "412592142851112960",#bot_practice
+                    "397794862755348480",#PUBG
+                    "393334968488820739",#mc_chatroom
+                    "397790090006364170",#monster hunter
+                ]
         if message.content.startswith("test0"):
             await client.delete_message(message)
 
-
-    channels = ["398447971018211329", "394434155443650560", "398446399005655051"]
+#Twitch自動削除
         elif message.channel.id in channels:  #チャンネル指定
             if message.content.startswith("http"):
                 m = "**Good luck!** \n配信の書き込みは__１時間後__に自動削除されます。"  #BOT送信メッセージ
@@ -28,12 +36,7 @@ async def on_message(message):
                 await client.delete_messages([message, response])
 
 
-    Xchannels = [
-            "412592142851112960",#bot_practice
-            "397794862755348480",#PUBG
-            "393334968488820739",#mc_chatroom
-            "397790090006364170",#monster hunter
-    ]
+#役職付与
         elif message.channel.id in Xchannels:
                 if message.content.startswith("!register"):
                     team_dict = {
