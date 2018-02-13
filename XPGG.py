28794822b6f5fa56ef4ffca1a5f1fc0c5d4e4c19
jukaiof <reaju.sh1.ne.jp@gmail.com>
2018-02-14 06:04:20 +0900

    IFIF2

--- XPGG.py
+++ XPGG.py
@@ -18,6 +18,7 @@ async def on_message(message):
         if message.content.startswith("test0"):
             await client.delete_message(message)
 
+
     channels = ["398447971018211329", "394434155443650560", "398446399005655051"]
         elif message.channel.id in channels:  #チャンネル指定
             if message.content.startswith("http"):
@@ -26,33 +27,34 @@ async def on_message(message):
                 await asyncio.sleep(3600) #非同期処理
                 await client.delete_messages([message, response])
 
-    channels = [
-                "412592142851112960",#bot_practice
-                "397794862755348480",#PUBG
-                "393334968488820739",#mc_chatroom
-                "397790090006364170",#monster hunter
-        ]
-    elif message.channel.id in channels:
-            if message.content.startswith("!register"):
-                team_dict = {
-                        "pubg":"PUBG",
-                        "minecraft":"私立XP女学院マイクラ部",
-                        "mc":"私立XP女学院マイクラ部",
-                        "hunter":"hunter",
-                        "HUNTER":"hunter",
-                        "ハンター":"hunter",
-                }
-                entered_team = message.content[10:].lower()
-                if entered_team in team_dict:
-                    role = discord.utils.get(message.server.roles, name=team_dict[entered_team])
-                    for r in message.author.roles:
-                        if r.name == team_dict[entered_team]:
-                            await client.send_message(message.channel, "もう登録されてるよー。")
-                            return
-                    await client.add_roles(message.author, role)
-                    await client.send_message(message.channel, "<@%s> が **{0}**のメンバーになりました！" .format(role.name) % (message.author.id))
-                else:
-                    await client.send_message(message.channel, "役職名が間違っているか、その役職はこの処理では付与できません。")
+
+    Xchannels = [
+            "412592142851112960",#bot_practice
+            "397794862755348480",#PUBG
+            "393334968488820739",#mc_chatroom
+            "397790090006364170",#monster hunter
+    ]
+        elif message.channel.id in Xchannels:
+                if message.content.startswith("!register"):
+                    team_dict = {
+                            "pubg":"PUBG",
+                            "minecraft":"私立XP女学院マイクラ部",
+                            "mc":"私立XP女学院マイクラ部",
+                            "hunter":"hunter",
+                            "HUNTER":"hunter",
+                            "ハンター":"hunter",
+                    }
+                    entered_team = message.content[10:].lower()
+                    if entered_team in team_dict:
+                        role = discord.utils.get(message.server.roles, name=team_dict[entered_team])
+                        for r in message.author.roles:
+                            if r.name == team_dict[entered_team]:
+                                await client.send_message(message.channel, "もう登録されてるよー。")
+                                return
+                        await client.add_roles(message.author, role)
+                        await client.send_message(message.channel, "<@%s> が **{0}**のメンバーになりました！" .format(role.name) % (message.author.id))
+                    else:
+                        await client.send_message(message.channel, "役職名が間違っているか、その役職はこの処理では付与できません。")
 
 
 client.run(settings.token)
