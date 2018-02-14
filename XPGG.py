9b92b23afaa2ea0a2ddcecf773402f77bcf58b54
jukaiof <reaju.sh1.ne.jp@gmail.com>
2018-02-15 04:49:20 +0900

    完全動作版　v1.1

--- XPGG.py
+++ XPGG.py
@@ -26,10 +26,9 @@ async def on_message(message):
         ]
         if message.content.startswith("test0"):
             await client.delete_message(message)
+
         elif message.content.startswith("!flac"):
             await client.send_message(message.channel, ":thinking_face:")
-        else:
-            pass
 
 #Twitch自動削除
 
