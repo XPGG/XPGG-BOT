aebeec6b652814ffc7be95f97a108edcf5595631
jukaiof <reaju.sh1.ne.jp@gmail.com>
2018-02-17 21:48:15 +0900

    わけわからんようになった

--- XPGG.py
+++ XPGG.py
@@ -1,9 +1,7 @@
-# dev1.1.2 チャンネルの指定が競合して他のコマンドが入力できないバグを修正
 # coding: utf-8
 import discord
 import asyncio
 import settings
-import random
 
 client = discord.Client()
 
@@ -20,50 +18,34 @@ async def on_ready():
 async def on_message(message):
 
     # test0
-
-    if not message.author.bot:  # 他BOT回避
-        bot_practice = ["41259214285112960"]
+    if client.user != message.author:  # 他BOT回避
         channels = ["398447971018211329", "394434155443650560", "398446399005655051"]
         Xchannels = [
-                    "397794862755348480",   # PUBG
-                    "393334968488820739",   # mc_chatroom
-                    "397790090006364170",   # monster hunter
+                    "412592142851112960",  # bot_practice
+                    "397794862755348480",  # PUBG
+                    "393334968488820739",  # mc_chatroom
+                    "397790090006364170",  # monster hunter
         ]
-        pubg_channels = ["397794862755348480"]
         if message.content.startswith("test0"):
             await client.delete_message(message)
 
-        elif message.content.startswith("!pubg "):
-            if message.channel.id in pubg_channels or bot_practice:
-                entered_map = ["Is", "de"]
-                Island_list = [
-                                "Pochinki",
-                                "Militery Base",
-                                "School",
-                                "Mansion",
-                                "Rozhok",
-                                "Yasnaya Polynaya",
-                                "Georgopol",
-                                "Prison or Hospital",
-                ]
-                entered_select = message.content[6:]
-                if entered_select == entered_map[0]:
-                    await client.send_message(message.channel, "おすすめの降下地点は " + "**__" + str(random.choice(Island_list)) + "__** MAP:Island")
-        # Twitch自動削除
+        elif message.content.startswith("!flac"):
+            await client.send_message(message.channel, ":thinking_face:")
 
-        elif "http" in message.content:   # チャンネル指定
-            if message.channel.id in channels or bot_practice:   # もしメッセージの中に”http”が入ってたら
+            # Twitch自動削除
+
+        elif message.channel.id in channels:  # チャンネル指定
+            if "http" in message.content:   # もしメッセージの中に”http”が入ってたら
                 m = "**Good luck!** \n配信の書き込みは__１時間後__に自動削除されます。"  # BOT送信メッセージ関数
                 response = await client.send_message(message.channel, m)    # 返信場所指定
-                await asyncio.sleep(3600)   # 非同期処理
+                await asyncio.sleep(3600)  # 非同期処理
                 await client.delete_messages([message, response])
             else:
                 pass
 
         # 役職付与
-
-        elif message.content.startswith("!register"):
-                if message.channel.id in Xchannels or bot_practice:
+        elif message.channel.id in Xchannels:
+                if message.content.startswith("!register"):
                     team_dict = {
                             "pubg": "PUBG",
                             "minecraft": "私立XP女学院マイクラ部",
@@ -85,7 +67,6 @@ async def on_message(message):
                         await client.send_message(message.channel, "役職名が間違っているか、その役職はこの処理では付与できません。")
                 else:
                     pass
-
         else:
             pass
 
